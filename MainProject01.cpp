5b4681d0941f9dbea0df45395485c7086089f7d4
MegaClinton <clinton3122003@gmail.com>
2022-10-02 21:50:41 -0500

    aaaa

---
+++ MainProject01.cpp
@@ -0,0 +1,16 @@
+//
+// Created by clinton on 10/2/2022.
+//
+
+#include "MainProject01.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+
+    return 0;
+}
---
+++ MainProject01.h
@@ -0,0 +1,14 @@
+//
+// Created by clinton on 10/2/2022.
+//
+
+#ifndef PROJECT_01_CSCE_1030_MAINPROJECT01_H
+#define PROJECT_01_CSCE_1030_MAINPROJECT01_H
+
+
+class MainProject01 {
+
+};
+
+
+#endif //PROJECT_01_CSCE_1030_MAINPROJECT01_H

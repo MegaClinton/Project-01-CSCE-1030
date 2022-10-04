7a0ae1d319d422043f41da78bfeeb6ca6c861adc
MegaClinton <clinton3122003@gmail.com>
2022-10-03 22:28:14 -0500

    SQUARE

--- cdn0116Project1.cpp
+++ cdn0116Project1.cpp
@@ -21,17 +21,34 @@ int main()
         cout << "Enter size: "; cin >> length;
     }
 
+    do
+    {
+        srand(time(NULL)); //seed random number generator
+        char ASCIIchar = (rand() % 32) + 33; //ASCII character to be used for shape generation
 
-    srand(time(NULL)); //seed random number generator
-    char ASCIIchar = (rand() % 32) + 33;
-
-    do {
-        cout << "What shape do you want to generate:\n1.Square\n2.Triange\n3.Pentagon\n4.Sentence\n5.Quit" << endl;
+        cout << "What shape do you want to generate:\n1.Square\n2.Triangle\n3.Pentagon\n4.Sentence\n5.Quit" << endl;
         cout << "Enter your choice: "; cin >> choice;
+
         switch (choice)
         {
             case (Square): //user entered 1
             {
+                system("clear"); //clear all output to generate first square
+                for (int i = 0; i <= length; i++) //regenerate squares by incrementing length
+                {
+                    system("clear"); //clear output each time new generation occurs
+                    int subLength = i + 1; //generate squares of incremental length according to i
+                    for(int h = 1; h < subLength; h++) //generate vertical row
+                    {
+                        cout << ASCIIchar;
+                        for(int v = 1; v < subLength - 1; v++) //generate horizontal row for every vertical row
+                        {
+                            cout << ASCIIchar;
+                        }
+                        cout << endl;
+                    }
+                    sleep(1); //delay after each generated square
+                }
                 break;
             }
             case (Triangle): //user entered 2
@@ -49,7 +66,7 @@ int main()
             case (Quit): //user entered 5
             {
                 cout << "Game over." << endl;
-                break;
+                return 0;
             }
             default: //user entered an incorrect choice
             {
@@ -67,12 +84,10 @@ int main()
         }
         else
         {
-            break;
+            return 0; //ends the program if user does not choose to repeat
         }
     }
     while(true);
-
-
 
     return 0;
 }

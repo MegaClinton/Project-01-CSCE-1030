068137076d4dbe509600ca206d3696741cbb2e1b
SpartanB01198 <47087991+SpartanB01198@users.noreply.github.com>
2022-10-09 14:47:17 -0500

    Update pentagon.cpp

--- pentagon.cpp
+++ pentagon.cpp
@@ -1,57 +1,46 @@
 #include <iostream>
+#include <unistd.h>
 using namespace std;
-	
-	
-int main ()
+
+int main()
 {
-int i, j;
-int length;
-cout<<"Enter length of the shape:";
-cin>> length;
-srand(time(NULL)); //seed random number generator
-        char ASCIIchar = (rand() % 32) + 33; 
-// trianlge part
-	for(i =1;i <= length-1;i++)
+    int length;
+    cout<<"Enter length of the shape:";
+    cin>>length;
+    srand(time(NULL)); //seed random number generator
+    
+    char ASCIIchar = (rand() % 32) + 33;
+    
+    //Variable to initialize the number of spaces in the first line.
+    //This is also used to track the number of spaces in subsequent lines.
+    int spaces = length+(length-1);
+    
+    for(int i=0; i < length; i++) //Update '<='operator to '<' so the lines match the input
+    {
+        //For loop to add spaces.
+	 
+        for (int s=1; s<spaces; s++)
+        {
+		          
+		 cout <<" ";
+        }
+        for (int j=1; j<=((i*2)+1);j++)
+        {
+            cout <<" "<<ASCIIchar;
+        }
+        cout<< endl;
+        sleep(1); 
+        spaces=spaces-2; //Decrement number of spaces by 2
+    }
+
+	for(int i=1; i<=length;i++)
 	{
-		for(j=0;j<=length;j++) // horizontal adjustment
+		for(int w =1; w<=length;w++)
 		{
-			int subLength =0;
-			if((i+j)>=(length+1))
-			{
-				cout<<ASCIIchar << " ";
-			}
-			else
-			{
-				cout<<" ";
-			} 
-		
+			cout<<" "<< ASCIIchar;
 		}
 		cout<<endl;
 	}
-// rectangle part
-	 for(int p=1; p<=length;p++)
-	{
-		for(int c =1; c<=length +1;c++)
-		{
-			cout << ASCIIchar << " ";
-		}
-		cout<<endl;
-	} 
-	
-		
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-	return 0;
+	sleep(1);
+    return 0;
 }

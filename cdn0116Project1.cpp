cd7bed3225c27613b7ace3894c85436b42fc9a29
SpartanB01198 <47087991+SpartanB01198@users.noreply.github.com>
2022-10-09 15:16:27 -0500

    Update cdn0116Project1.cpp

--- cdn0116Project1.cpp
+++ cdn0116Project1.cpp
@@ -58,6 +58,31 @@ int main()
             }
             case (Pentagon): //user entered 3
             {
+                system("clear");
+                 for(int i=0; i < length; i++) //
+                  {
+                         //For loop to add spaces.
+                        for (int s=1; s<length-i; s++)
+	                     {
+	                     	cout<<" ";
+	                     }
+	                   	// the increments of 2 for each line.
+	                  	for( int j =1; j<=((i*2)+1);j++)
+	                  	{
+		                     	cout<<ASCIIchar;
+		                  }
+		                  cout<<endl;
+		                  sleep(1);
+                       }
+	            for(int l = 1; l<=length-1;l++) //length of the rectangle.
+	             {
+	            	for(int w=1;w<=length*2-1;w++) // width of the rectangle to match the triangle.
+		           {
+		            	cout<<ASCIIchar;
+		            }
+	            	cout<<endl;
+	            	sleep(1);
+               }
                 break;
             }
             case (Sentence): //user entered 4

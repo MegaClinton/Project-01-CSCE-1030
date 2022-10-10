1fdd75257025d353da53ec737b2f5d689be1f25a
GucciMacaroni <aaron.millman@yahoo.com>
2022-10-09 23:19:13 -0500

    Update AaronProject01.cpp

--- AaronProject01.cpp
+++ AaronProject01.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
-using namespace std;
+#include <unistd.h>
+    using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int main()
+    {
+        int length;
+        cout<<"Enter length of the shape:";
+        cin>>length;
+        srand(time(NULL)); //seed random number generator
+        system("clear");
+        char ASCIIchar = (rand() % 32) + 33;
+
+        for(int i=0; i < length; i++) //
+        {
+            //For loop to add spaces.
+            for (int s=1; s<length-i; s++)
+            {
+                cout<<" ";
+            }
+            // the increments of 2 for each line.
+            for( int j =1; j<=((i*2)+1);j++)
+            {
+                cout<<ASCIIchar;
+            }
+            cout<<endl;
+            sleep(1);
+        }
     return 0;
 }
